e70a928a2aabf3269ca0b1f087ee835f30b48c04
Mikhail Oskilko <311707@niuitmo.ru>
2024-02-26 22:03:07 +0300

    add more tests

--- test/all_test.clj
+++ test/all_test.clj
@@ -1,5 +1,5 @@
 (ns all-test
-  (:require [clojure.test :refer [deftest testing is run-tests run-test]])
+  (:require [clojure.test :refer [deftest testing is run-tests]])
   (:require core)
   (:require linear)
   (:require utils)
@@ -8,13 +8,28 @@
 (defn approximate? [expected actual tolerance]
   (<= (Math/abs (- expected actual)) tolerance))
 
+(def dot-apr 0.01)
 (def two-point [[1 1] [3 3]])
 (def point-to-search 2)
 (def actual-y 2)
 
 (deftest lagrange
   (testing "find-one-point"
-    (is true)))
+    (let [array-points [[1 1] [2 5] [10 25] [16 18]]
+          point 9
+          actual 24.02
+          result (lagrange/find-lagrange-interpolation array-points point)]
+      (is (approximate? actual result dot-apr))))
+  (testing "many-dots"
+    (let [array-points-2 [[1. 1.] [2. 5.] [10. 25.] [16. 18.]]
+          f 1
+          result-2 (lagrange/lagrange-interpolation array-points-2 f)
+          actual-2 [[1.0 1.0] [2.0 5.0] [3.0 8.76] [4.0 12.23] [5.0 15.38] [6.0 18.17] [7.0 20.57] [8.0 22.53] [9.0 24.02] [10.0 25.0] [11.0 25.43] [12.0 25.27] [13.0 24.49] [14.0 23.04] [15.0 20.89] [16.0 18.0]]]
+
+      (is (= (count result-2) (+ (- 16 1) 1)))
+
+      (doseq [i (range (count result-2))]
+        (is (approximate? (second (nth result-2 i)) (second (nth actual-2 i)) dot-apr))))))
 
 (deftest linear
   (testing "test-one-value"
@@ -54,5 +69,4 @@
           result (utils/map-double (conj (vec (range 0. 5. 0.5)) 5.))]
       (is (= actual result)))))
 
-(run-test utils)
 (run-tests)
